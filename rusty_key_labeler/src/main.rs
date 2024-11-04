dfd8755bccbcd14a8a7ca5cf3a203adc5ea868a3
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2024-11-04 08:46:06 -0600

    Starting over.

--- rusty_key_labeler/src/main.rs
+++ rusty_key_labeler/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, io::Write};
+mod settings;
 
 use bevy::{
     color::palettes::css::RED,
@@ -10,11 +10,14 @@ use bevy::{
         },
         view::RenderLayers,
     },
-    sprite::{Material2d, MaterialMesh2dBundle, Mesh2d, Mesh2dHandle},
+    window::WindowResized,
 };
-mod settings;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_prototype_lyon::prelude::*;
+use std::fs;
+
 use settings::Config;
-use yolo_io::{ImageLabelPair, YoloDataQualityReport, YoloFile, YoloProject};
+use yolo_io::{ImageLabelPair, YoloFile, YoloProject};
 
 #[derive(Resource, Debug, Clone)]
 pub struct YoloProjectResource(YoloProject);
@@ -25,16 +28,15 @@ pub struct AppData {
     current_pair: Option<ImageLabelPair>,
 }
 
-#[derive(Component)]
-pub struct CurrentImage;
+// const CANVAS_LAYER: RenderLayers = RenderLayers::layer(0);
+// const IMAGE_LAYER: RenderLayers = RenderLayers::layer(1);
+// const BOUNDING_BOX_LAYER: RenderLayers = RenderLayers::layer(2);
+const RESOLUTION_WIDTH: f32 = 1920.0;
+const RESOLUTION_HEIGHT: f32 = 1080.0;
 
 /*
 TODO: Resize image to fit window
 */
-
-const PIXEL_PERFECT_LAYERS: RenderLayers = RenderLayers::layer(0);
-/// Render layers for high-resolution rendering.
-const HIGH_RES_LAYERS: RenderLayers = RenderLayers::layer(1);
 
 fn main() {
     // Load YAML configuration file from file.
@@ -63,121 +65,251 @@ fn main() {
     };
 
     App::new()
-        .add_plugins(DefaultPlugins)
+        .init_resource::<Assets<ColorMaterial>>()
+        .add_plugins((
+            // DefaultPlugins,
+            ShapePlugin,
+            DefaultPlugins.set(ImagePlugin::default_nearest()),
+            WorldInspectorPlugin::new(),
+        ))
         .insert_resource(config)
         .insert_resource(project_resource)
         .insert_resource(app_data)
+        // Add color resources
         .add_systems(Startup, setup)
         .add_systems(
             Update,
-            (zoom_system, translate_image_system, load_next_image_system),
+            (
+                zoom_system,
+                translate_image_system,
+                load_next_image_system,
+                // load_labels_for_image_system,
+                // fit_canvas_system,
+            ),
         )
         .run();
 }
 
-#[derive(Debug, Clone)]
-pub struct BevyYoloFile(pub YoloFile);
+// #[derive(Debug, Clone, Component)]
+// pub struct BevyYoloFile(pub YoloFile);
 
 // Impl conversion of YoloFile to MaterialMesh2dBundle
-impl BevyYoloFile {
-    fn to_2d_bundle(
-        &self,
-        // image: &mut ResMut<Assets<Mesh>>,
-        meshes: &mut ResMut<Assets<Mesh>>,
-        materials: &mut ResMut<Assets<ColorMaterial>>,
-    ) -> Vec<MaterialMesh2dBundle<ColorMaterial>> {
-        let mut bundles = Vec::new();
-        let yolo_file = self.0.clone();
 
-        for entry in yolo_file.entries.iter() {
-            let x_center = entry.x_center;
-            let y_center = entry.y_center;
-            let width = entry.width;
-            let height = entry.height;
+#[derive(Debug, Clone, Component)]
+pub struct Canvas;
 
-            let rectangle_handle = Mesh2dHandle(meshes.add(Rectangle::new(width, height)));
+#[derive(Component)]
+pub struct ImageToLoad;
 
-            let color_material = ColorMaterial::from_color(RED);
+#[derive(Debug, Clone, Component)]
+pub struct BevyYoloFile(pub YoloFile);
 
-            let bundle = MaterialMesh2dBundle {
-                mesh: rectangle_handle,
-                material: materials.add(color_material),
-                transform: Transform::from_xyz(x_center, y_center, 0.0),
-                ..Default::default()
-            };
+// #[derive(Debug, Clone, Component)]
+// pub struct YoloLabelBundle {
+//     spatial_bundle: SpatialBundle,
+//     rectangle: shapes::Rectangle,
+// }
 
-            bundles.push(bundle);
-        }
+#[derive(Debug, Clone, Component)]
+pub struct CanvasCamera;
 
-        bundles
-    }
-}
+// #[derive(Debug, Clone, Component)]
+// pub struct ImageCamera;
+
+// impl BevyYoloFile {
+//     fn to_rectangle(&self, image_size: Vec2) -> Vec<YoloLabelBundle> {
+//         let mut bundles: Vec<YoloLabelBundle> = Vec::new();
+//         let yolo_file = self.0.clone();
+
+//         for entry in yolo_file.entries.iter() {
+//             let scaled_x_center = entry.x_center * image_size.x;
+//             let scaled_y_center = entry.y_center * image_size.y;
+//             let scaled_width = entry.width * image_size.x;
+//             let scaled_height = entry.height * image_size.y; // Shape::Rectangle(Rectangle::new(80., 80.))
+
+//             // Create a 2d rectangle
+//             let spatial_rectangle_bundle = YoloLabelBundle {
+//                 spatial_bundle: SpatialBundle {
+//                     transform: Transform::from_translation(Vec3::new(
+//                         scaled_x_center,
+//                         scaled_y_center,
+//                         0.,
+//                     )),
+//                     visibility: Visibility::Visible,
+//                     ..Default::default()
+//                 },
+//                 rectangle: shapes::Rectangle {
+//                     extents: Vec2::new(scaled_width, scaled_height),
+//                     origin: shapes::RectangleOrigin::Center,
+//                 },
+//             };
+
+//             bundles.push(spatial_rectangle_bundle);
+//         }
+
+//         bundles
+//     }
+// }
 
 // WILO: Trying to load image, get size, and then draw bounding boxes
 //       to scale on the image.
+
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    project_resource: Res<YoloProjectResource>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
     mut images: ResMut<Assets<Image>>,
-) {
-    let valid_pairs = project_resource.0.get_valid_pairs();
-
-    let first_image = valid_pairs.first().unwrap();
-    let image_handle: Handle<Image> = asset_server.load(first_image.image_path.clone().unwrap());
-
-    let label_file = first_image.label_file.clone().unwrap();
-    let bevy_yolo_file = BevyYoloFile(label_file);
-
-    // Get the size of the image
-    let image = images.get(&image_handle).unwrap();
-    let image_size = Vec2::new(image.width() as f32, image.height() as f32);
-
-    let shapes = bevy_yolo_file.to_2d_bundle(&mut meshes, &mut materials);
-
-    commands.spawn(Camera2dBundle::default());
-
-    commands.spawn(SpriteBundle {
-        // sprite: todo!(),
-        // transform: todo!(),
-        // global_transform: todo!(),
-        texture: image_handle,
-        // visibility: todo!(),
-        // inherited_visibility: todo!(),
-        // view_visibility: todo!(),
-        ..Default::default()
-    });
-}
-
-pub fn load_selected_image_system(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut app_data: ResMut<AppData>,
     project_resource: Res<YoloProjectResource>,
 ) {
+    let canvas_size = Extent3d {
+        width: RESOLUTION_WIDTH as u32,
+        height: RESOLUTION_HEIGHT as u32,
+        ..default()
+    };
+
+    let mut canvas = Image {
+        texture_descriptor: TextureDescriptor {
+            label: Some("canvas"),
+            size: canvas_size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::Bgra8UnormSrgb,
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+                | TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        },
+        ..default()
+    };
+
+    canvas.resize(canvas_size);
+    let canvas_handle = images.add(canvas);
+
+    // commands.spawn((
+    //     Name::new("canvas_camera"),
+    //     Camera2dBundle {
+    //         camera: Camera {
+    //             // render before the "main pass" camera
+    //             order: -1,
+    //             target: RenderTarget::Image(canvas_handle.clone()),
+    //             msaa_writeback: true,
+    //             ..default()
+    //         },
+    //         ..default()
+    //     },
+    //     CanvasCamera,
+    //     CANVAS_LAYER,
+    // ));
+
+    // Spawn the canvas
+    let canvas_eid = commands
+        .spawn((
+            Name::new("canvas"),
+            SpriteBundle {
+                texture: canvas_handle.clone(),
+                transform: Transform::from_translation(Vec3::new(
+                    RESOLUTION_WIDTH / 2.0,
+                    RESOLUTION_HEIGHT / 2.0,
+                    0.0,
+                )),
+                ..Default::default()
+            },
+            Canvas,
+        ))
+        .id();
+
+    // Load first image
     let valid_pairs = project_resource.0.get_valid_pairs();
-    let selected_pair = valid_pairs.get(app_data.index).unwrap();
-    let selected_image = selected_pair.image_path.clone().unwrap();
-    let selected_image = selected_image.as_path().to_string_lossy().into_owned();
+    let pair = valid_pairs.first().unwrap();
+    let image_handle: Handle<Image> = asset_server.load(pair.image_path.clone().unwrap());
 
-    // Remove current image
-    for entity in commands.query::<Entity>().with::<CurrentImage>().iter() {
-        commands.entity(entity).despawn();
-    }
+    let image_eid = commands
+        .spawn((
+            Name::new("image"),
+            SpriteBundle {
+                texture: image_handle.clone(),
+                transform: Transform::from_translation(Vec3::new(
+                    RESOLUTION_WIDTH / 2.0,
+                    RESOLUTION_HEIGHT / 2.0,
+                    1.0,
+                )),
+                ..Default::default()
+            },
+            ImageToLoad,
+        ))
+        .id();
 
+    // Set the image as a child of the canvas
+    commands.entity(canvas_eid).push_children(&[image_eid]);
+
+    // Create a camera for the image
     commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load(selected_image),
+        Name::new("image_camera"),
+        Camera2dBundle {
+            camera: Camera {
+                // order: 0,
+                target: RenderTarget::Image(canvas_handle.clone()),
+                ..Default::default()
+            },
+            transform: Transform::from_translation(Vec3::new(
+                RESOLUTION_WIDTH / 2.0,
+                RESOLUTION_HEIGHT / 2.0,
+                100.0,
+            )),
             ..Default::default()
         },
-        CurrentImage,
+        CanvasCamera,
     ));
-
-    app_data.current_pair = Some(selected_pair.clone());
 }
+
+// pub fn load_labels_for_image_system(
+//     mut commands: Commands,
+//     asset_server: Res<AssetServer>,
+//     app_data: ResMut<AppData>,
+//     project_resource: Res<YoloProjectResource>,
+//     images: ResMut<Assets<Image>>,
+//     query: Query<Entity, With<ImageToLoad>>,
+// ) {
+//     for entity in query.iter() {
+//         println!("Loading labels for image");
+//         let valid_pairs = project_resource.0.get_valid_pairs();
+//         let pair = valid_pairs[app_data.index].clone();
+
+//         let image_handle = asset_server
+//             .get_handle::<Image>(pair.image_path.clone().unwrap())
+//             .unwrap();
+
+//         if let Some(image) = images.get(&image_handle) {
+//             let image_size = Vec2::new(image.width() as f32, image.height() as f32);
+
+//             let yolo_file = pair.label_file.unwrap();
+//             let yolo_file = BevyYoloFile(yolo_file);
+
+//             let bundles = yolo_file.to_rectangle(image_size);
+
+//             for (index, bundle) in bundles.iter().enumerate() {
+//                 println!("Spawning bundle");
+//                 println!("Bundle position: {:#?}", bundle);
+
+//                 let bundle = (
+//                     Name::new(format!("label_{}", index)),
+//                     ShapeBundle {
+//                         path: GeometryBuilder::build_as(&bundle.rectangle),
+//                         spatial: bundle.spatial_bundle.clone(),
+//                         ..Default::default()
+//                     },
+//                     // Fill::color(REBECCA_PURPLE),
+//                     Stroke::new(RED, 3.0),
+//                 );
+
+//                 commands.spawn(bundle);
+//             }
+
+//             // Remove ImageToLoad component
+//             commands.entity(entity).remove::<ImageToLoad>();
+//         }
+//     }
+// }
 
 pub fn load_next_image_system(
     mut commands: Commands,
@@ -185,12 +317,12 @@ pub fn load_next_image_system(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut app_data: ResMut<AppData>,
     project_resource: Res<YoloProjectResource>,
-    query: Query<Entity, With<CurrentImage>>,
+    query: Query<Entity, With<ImageToLoad>>,
 ) {
     if keyboard_input.just_pressed(KeyCode::ArrowRight) {
         println!("Loading next image");
 
-        // Remove current image
+        // Remove ImageToLoad component
         for entity in query.iter() {
             commands.entity(entity).despawn();
         }
@@ -222,7 +354,7 @@ fn load_next_image(
                 texture: asset_server.load(next_image),
                 ..Default::default()
             },
-            CurrentImage,
+            ImageToLoad,
         ));
         app_data.index = next_index;
         app_data.current_pair = Some(valid_pairs[next_index].clone());
@@ -271,3 +403,16 @@ pub fn translate_image_system(
         transform.translation = translation;
     }
 }
+
+// Scales camera projection to fit the window (integer multiples only).
+// fn fit_canvas_system(
+//     mut resize_events: EventReader<WindowResized>,
+//     mut projections: Query<&mut OrthographicProjection, With<CanvasCamera>>,
+// ) {
+//     for event in resize_events.read() {
+//         let h_scale = event.width / RESOLUTION_WIDTH;
+//         let v_scale = event.height / RESOLUTION_HEIGHT;
+//         let mut projection = projections.single_mut();
+//         projection.scale = 1. / h_scale.min(v_scale).round();
+//     }
+// }
